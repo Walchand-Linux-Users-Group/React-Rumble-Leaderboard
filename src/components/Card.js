0f521b8ee19c3f62d87f8ac781cc4a00ba2cecd3
Swarada Bhosale <swaradapb113@gmail.com>
2023-04-14 21:24:50 +0530

    Updated Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,14 @@
 import React, { useState } from 'react'
-import { FaUserAlt } from 'react-icons/fa'
-import { HiFlag } from 'react-icons/hi'
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import '../App.css'
 import TechnoLink from './TechnoLink'
-import wlug from '../Image/meta.png'
 import list from '../Image/pencil-checking-off-golden-todo-260nw-2171134529 (3).png'
-// import list from '../Image/whitelist.png'
 import style from './Card.module.css'
 import techno from '../Image/technotweet.png'
 
 const Card = () => {
-  // const navigate = useNavigate()
   const [placeholder, setPlaceholder] = useState('Answer fragment')
-  const [metaId, setMetdaId] = useState('')
+  const [technoId, setTechnoId] = useState('')
   const [ansKey, setansKey] = useState('')
   const [status, setStatus] = useState('')
 
@@ -54,11 +48,11 @@ const Card = () => {
       btn.classList.remove('focus')
     }, 500)
     console.log({
-      email: metaId,
+      email: technoId,
       pass: ansKey,
     })
 
-    await fetchdata(metaId, ansKey)
+    await fetchdata(technoId, ansKey)
     setansKey('')
   }
 
@@ -81,11 +75,6 @@ const Card = () => {
           <a href='' target='_blank' className={style.logo}>
             <img src={list} />
           </a>
-          {/* 
-          <div className={style.heading}>
-            <h1>TECHNOTWEET 2K23</h1>
-          </div> */}
-
           <form action='#' className={style.form}>
             <div class={style.container}>
               <label class={style.label}>Techno ID</label>
@@ -95,8 +84,8 @@ const Card = () => {
                 autoComplete='off'
                 placeholder='Your Techno ID'
                 name='user-id'
-                value={metaId}
-                onChange={(e) => setMetdaId(e.target.value)}
+                value={technoId}
+                onChange={(e) => setTechnoId(e.target.value)}
                 required
               />
             </div>
@@ -108,8 +97,8 @@ const Card = () => {
                 autoComplete='off'
                 placeholder='Your password'
                 name='password'
-                value={metaId}
-                onChange={(e) => setMetdaId(e.target.value)}
+                value={ansKey}
+                onChange={(e) => setansKey(e.target.value)}
                 required
               />
             </div>
@@ -122,7 +111,6 @@ const Card = () => {
             <p style={{ color: 'black' }}>{status}</p>
           </div>
         </div>
-
         <button onClick={handleClick} className={style.leaderboardBtn}>
           See Leaderboard !
         </button>
