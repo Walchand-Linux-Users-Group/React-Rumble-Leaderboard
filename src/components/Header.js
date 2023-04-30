c8517e03c6ad1b5154b4324ea35173f11a8cd610
Swarada Bhosale <swaradapb113@gmail.com>
2023-04-30 10:08:02 +0530

    Changed logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from './Header.module.css'
-import logo from '../Image/WLUG_white.png'
+import logo from '../Image/WLUG_Black_Logo.png'
 
 const Header = () => {
   return (
@@ -11,7 +11,5 @@ const Header = () => {
     </>
   )
 }
-
-
 
 export default Header
